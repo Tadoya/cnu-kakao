42d8674b9a63054b5bf2ca324840e24cb03a393e
SeongSikChoi <whtemos@naver.com>
2017-09-16 14:09:38 +0900

    library update

--- cnucrawling/library_crawl.py
+++ cnucrawling/library_crawl.py
@@ -28,6 +28,9 @@ def crawl():
     for title in my_titles:
         data.append(title.text)
 
+    i=0
+    print(data[1].split())
+
     return make_library_json(data)
 
 
@@ -43,5 +46,5 @@ def make_library_json(data):
 #크롤링한 좌석정보를 Json화 할 때 key 값만들기 (ex: 2-1A)
 def make_library_json_key(data, i):
     if len(data[i].split()[3])==1:
-        return data[i].split()[0][0:1]+'-'+data[i].split()[2][0:1]+data[i].split()[3]
-    return data[i].split()[0][0:1] + '-' + data[i].split()[2][0:1] +' '+data[i].split()[3]
+        return data[i].split()[0] +' 제 '+ data[i].split()[2] + data[i].split()[3]
+    return data[i].split()[0] +' 제 '+ data[i].split()[2] + ' ' + data[i].split()[3]
